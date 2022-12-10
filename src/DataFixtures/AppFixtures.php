c49f1937b52ed48a7c122694961b17b25687f7fa
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-12-10 12:26:30 +0100

    Create fixtures for JobOffer

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\JobOffer;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,26 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 20; ++$i) {
+            $offer = new JobOffer();
+            $offer->setJobName($this->randomJobName());
+            $offer->setDescription('Lorem ipsum dolor sit amet');
+            $offer->setCreationDate(new \DateTimeImmutable());
+
+            $manager->persist($offer);
+        }
 
         $manager->flush();
+    }
+
+    private function randomJobName(): string
+    {
+        $seniority = ['Trainee', 'Junior', 'Regular', 'Senior', 'Lead'];
+        $tech = ['Node.js', 'PHP', 'Java', 'Frontend', '.NET'];
+
+        $randomSeniority = $seniority[array_rand($seniority)];
+        $randomTech = $tech[array_rand($tech)];
+
+        return "$randomSeniority $randomTech Developer";
     }
 }
