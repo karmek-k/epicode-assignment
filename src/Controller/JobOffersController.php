fe43631c685b0691777cb9bd78e235f070fe99f6
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-12-10 12:13:26 +0100

    Implement offer fetching in JobOffersController.php

--- src/Controller/JobOffersController.php
+++ src/Controller/JobOffersController.php
@@ -2,15 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\JobOfferRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class JobOffersController extends AbstractController
 {
     #[Route('/', name: 'app_job_offers')]
-    public function index(): Response
+    public function list(Request $req, JobOfferRepository $repo): Response
     {
-        return $this->render('job_offers/index.html.twig');
+        // TODO clean up
+        $maxDaysAgo = $req->query->get('maxDaysAgo');
+        $date = null;
+        if (!empty($maxDaysAgo)) {
+            $maxDaysAgo = (int) $maxDaysAgo;
+            $date = new \DateTimeImmutable("now - $maxDaysAgo");
+        }
+
+        return $this->render('job_offers/index.html.twig', [
+            'offers' => $repo->findNotOlderThan($date),
+        ]);
     }
 }
