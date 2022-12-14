c8fd4560c009d295da01c7bb47dd964917601a14
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-12-14 17:29:12 +0100

    Update ApplicantCv.php to use the uploader bundle

--- src/Entity/ApplicantCv.php
+++ src/Entity/ApplicantCv.php
@@ -5,8 +5,11 @@ namespace App\Entity;
 use App\Repository\ApplicantCvRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: ApplicantCvRepository::class)]
+#[Vich\Uploadable]
 class ApplicantCv
 {
     #[ORM\Id]
@@ -17,6 +20,12 @@ class ApplicantCv
 
     #[ORM\Column(length: 255)]
     private ?string $fileName = null;
+
+    #[Vich\UploadableField(mapping: 'cv', fileNameProperty: 'fileName')]
+    private ?File $file = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
 
     public function getId(): ?string
     {
@@ -31,6 +40,41 @@ class ApplicantCv
     public function setFileName(string $fileName): self
     {
         $this->fileName = $fileName;
+
+        return $this;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     */
+    public function setFile(?File $file = null): void
+    {
+        $this->file = $file;
+
+        if ($file !== null) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getFile(): ?File
+    {
+        return $this->file;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
