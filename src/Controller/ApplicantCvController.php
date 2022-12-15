c55ef4a5320c2e9509d45689abe7f91a74536900
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-12-15 19:47:38 +0100

    Create ApplicantCvController and index template

---
+++ src/Controller/ApplicantCvController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Applicant;
+use App\Entity\ApplicantCv;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Vich\UploaderBundle\Handler\DownloadHandler;
+
+class ApplicantCvController extends AbstractController
+{
+    #[Route('/cv', name: 'app_applicantcv')]
+    public function index(): Response
+    {
+        /** @var Applicant $user */
+        $user = $this->getUser();
+
+        return $this->render('applicant_cv/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/cv/{cv}', name: 'app_applicantcv_download')]
+    public function download(ApplicantCv $cv, DownloadHandler $downloadHandler): Response
+    {
+        return $downloadHandler->downloadObject($cv, 'file', forceDownload: false);
+    }
+}
